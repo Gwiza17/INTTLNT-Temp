e399f09d9e980a38e420528d8a5738bf9f6c7d71
prosper-codes <tpjn02@gmail.com>
2026-02-27 11:42:59 +0100

    modify middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -52,6 +52,7 @@ export async function middleware(request: NextRequest) {
       .select('roles, status')
       .eq('user_id', user.id)
       .maybeSingle()
+    
 
     console.log('user.id:', user.id)
     console.log('stakeholder:', JSON.stringify(stakeholder))
